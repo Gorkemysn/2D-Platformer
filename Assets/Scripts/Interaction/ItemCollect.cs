0924ea169814391cb3fd9b2ea85ad98a38af0f33
Görkem Yasin <yasin_gul_2001@hotmail.com>
2024-01-01 00:28:05 +0300

    Item Collect_UI

--- Assets/Scripts/Interaction/ItemCollect.cs
+++ Assets/Scripts/Interaction/ItemCollect.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ItemCollect : MonoBehaviour
 {
     private int rings = 0;
+    [SerializeField] private TextMeshProUGUI ringText;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,6 +15,7 @@ public class ItemCollect : MonoBehaviour
         {
             Destroy(collision.gameObject);
             rings++;
+            ringText.text = (int.Parse(ringText.text) + 1).ToString();
         }
     }
 }
