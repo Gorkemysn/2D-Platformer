d1c0550f0b286a7f33de11c6f378544f6a0c51e5
Görkem Yasin <yasin_gul_2001@hotmail.com>
2024-01-02 22:06:31 +0300

    MainMenu and PauseMenu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainScreen;
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        Application.LoadLevel("Level_1");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
